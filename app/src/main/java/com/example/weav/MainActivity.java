a7f44ad7c173275d9b25f09be2c7e05db29f1e19
lily-xy <757560761@qq.com>
2022-03-20 15:53:40 +0800

    add the function of input password and check whether the name and password are already input

--- app/src/main/java/com/example/weav/MainActivity.java
+++ app/src/main/java/com/example/weav/MainActivity.java
@@ -8,11 +8,13 @@ import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.EditText;
+import android.widget.LinearLayout;
 import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
 
     private EditText inputName;
+    private EditText inputPsw;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -26,13 +28,20 @@ public class MainActivity extends AppCompatActivity {
     public void onClickButtonStart(View view) {
 
         inputName =(EditText) findViewById(R.id.editTextTextPersonName);
+        inputPsw = (EditText) findViewById(R.id.Password);
         String userName = inputName.getText().toString();
+        String psw = inputPsw.getText().toString();
+
 
         if (userName.isEmpty()) {
-            Toast.makeText(this, "Please input your name first", Toast.LENGTH_LONG).show();
+            Toast.makeText(this, "Please input your name", Toast.LENGTH_LONG).show();
+            return;
+        } else if (psw.isEmpty()){
+            Toast.makeText(this, "Please input your password", Toast.LENGTH_LONG).show();
             return;
         }
         inputName.setText("");
+        inputPsw.setText("");
 
         Intent intent = new Intent(this, WelcomeActivity.class);
         intent.putExtra("userName", userName);
