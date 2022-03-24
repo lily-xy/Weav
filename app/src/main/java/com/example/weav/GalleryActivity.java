cc078856f230e09d9ab08843c3c6c2244f00f596
lily-xy <757560761@qq.com>
2022-03-24 19:22:12 +0800

    add changImage function to the bird image

--- app/src/main/java/com/example/weav/GalleryActivity.java
+++ app/src/main/java/com/example/weav/GalleryActivity.java
@@ -1,17 +1,31 @@
 package com.example.weav;
 
+import androidx.appcompat.app.ActionBar;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.constraintlayout.widget.ConstraintLayout;
 
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.ImageView;
 
 public class GalleryActivity extends AppCompatActivity {
+
+    Boolean isChoseImg = false;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_gallery);
+
+        ActionBar actionBar = getSupportActionBar();
+        actionBar.hide();
+
+        int uiOptions = View.SYSTEM_UI_FLAG_FULLSCREEN
+                | View.SYSTEM_UI_FLAG_HIDE_NAVIGATION
+                | View.SYSTEM_UI_FLAG_IMMERSIVE_STICKY;
+        getWindow().getDecorView().setSystemUiVisibility(uiOptions);
+
     }
 
     public void onClickButtonTakePhoto(View view) {
@@ -36,6 +50,20 @@ public class GalleryActivity extends AppCompatActivity {
         startActivity(intent);
     }
 
+    public void changeImageState(View view) {
 
+        ImageView img = findViewById(R.id.bird_Img);
+
+        if(img.getTag().equals("unChosen")){
+            isChoseImg = true;
+            img.setTag("chosen");
+            img.setImageResource(R.drawable.chosenbird);
+        }else{
+            isChoseImg = false;
+            img.setTag("unChosen");
+            img.setImageResource(R.drawable.first_2);
+        }
+
+    }
 
 }
