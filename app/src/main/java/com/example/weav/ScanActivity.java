bb7638e9b9185e0225649c33109e20cb29ee1de5
lily-xy <757560761@qq.com>
2022-03-24 20:51:43 +0800

    add a class to draw the scanLine

--- app/src/main/java/com/example/weav/ScanActivity.java
+++ app/src/main/java/com/example/weav/ScanActivity.java
@@ -2,11 +2,86 @@ package com.example.weav;
 
 import androidx.appcompat.app.ActionBar;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.constraintlayout.widget.ConstraintLayout;
 
+import android.content.Context;
+import android.content.Intent;
+import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
+import android.graphics.Canvas;
+import android.graphics.Paint;
+import android.graphics.Rect;
+import android.graphics.RectF;
 import android.os.Bundle;
+import android.view.GestureDetector;
 import android.view.View;
 
 public class ScanActivity extends AppCompatActivity {
+
+    scanLine line;
+
+    public class scanLine {
+        int vX = 0;
+        int vY = 5;
+        private float locX;
+        private float locY;
+
+        Paint paint;
+        Bitmap bitmap = BitmapFactory.decodeResource(getResources(), R.drawable.line);
+
+        public scanLine() {
+            this.paint = new Paint();
+            locX = 85;
+            locY = 200;
+        }
+
+        public float getlocX() {
+            return locX;
+        }
+
+        public void setlocX(float locX) {
+            this.locX = locX;
+        }
+
+        public float getlocY() {
+            return locY;
+        }
+
+        public void setlocY(float locY) {
+            this.locY = locY;
+        }
+
+    }
+
+    public class GraphicsView extends View {
+
+        public GraphicsView(Context context) {
+            super(context);
+            line = new scanLine();
+        }
+
+
+        public void isReach(){
+            float reachHeight = getHeight()/6*5;
+
+            if(line.getlocY() >= reachHeight)
+                line.vY = -line.vY;
+
+            if (line.getlocY() < 100)
+                jumpNext(this);
+            }
+
+        public void onDraw(Canvas canvas) {
+
+            super.onDraw(canvas);
+
+            isReach();
+            line.locY += line.vY;
+            canvas.drawLine(line.getlocX(), line.getlocY(), getWidth() - line.getlocX(), line.getlocY()+5, line.paint);
+
+            invalidate();
+        }
+    }
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -16,9 +91,21 @@ public class ScanActivity extends AppCompatActivity {
         ActionBar actionBar = getSupportActionBar();
         actionBar.hide();
 
-        int uiOptions = View.SYSTEM_UI_FLAG_FULLSCREEN
-                | View.SYSTEM_UI_FLAG_HIDE_NAVIGATION
-                | View.SYSTEM_UI_FLAG_IMMERSIVE_STICKY;
-        getWindow().getDecorView().setSystemUiVisibility(uiOptions);
+        ConstraintLayout root = findViewById(R.id.scanView);
+        GraphicsView lineView = new GraphicsView(this);
+        root.addView(lineView);
+
+        line = new scanLine();
+    }
+
+    public void jumpNext(View view) {
+
+        Intent getIntent = getIntent();
+        String userName = getIntent.getStringExtra("userName");
+
+        Intent intent = new Intent(this, DealActivity.class);
+        intent.putExtra("userName", userName);
+
+        startActivity(intent);
     }
 }
