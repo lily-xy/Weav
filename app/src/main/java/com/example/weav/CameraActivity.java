1b7e13e5e5647690434eb275325200307646a9ae
lily-xy <757560761@qq.com>
2022-03-24 15:52:46 +0800

    add function to gallery button in Camera page

--- app/src/main/java/com/example/weav/CameraActivity.java
+++ app/src/main/java/com/example/weav/CameraActivity.java
@@ -2,7 +2,9 @@ package com.example.weav;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
 
 public class CameraActivity extends AppCompatActivity {
 
@@ -10,6 +12,17 @@ public class CameraActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_camera);
+    }
+
+    public void onClickButtonGallery(View view) {
+
+        Intent getIntent = getIntent();
+        String userName = getIntent.getStringExtra("userName");
+
+        Intent intentGallery = new Intent(this, GalleryActivity.class);
+        intentGallery.putExtra("userName", userName);
+
+        startActivity(intentGallery);
     }
 
 }
