38e90409a0da52a3cf77f48116dc96415fc3edcf
lily-xy <757560761@qq.com>
2022-03-25 19:17:38 +0800

    change the delay time

--- app/src/main/java/com/example/weav/buildingActivity.java
+++ app/src/main/java/com/example/weav/buildingActivity.java
@@ -2,11 +2,18 @@ package com.example.weav;
 
 import androidx.appcompat.app.ActionBar;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.constraintlayout.widget.ConstraintLayout;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.os.Handler;
 import android.view.View;
 
+import java.util.Timer;
+import java.util.TimerTask;
+
 public class buildingActivity extends AppCompatActivity {
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -20,5 +27,28 @@ public class buildingActivity extends AppCompatActivity {
                 | View.SYSTEM_UI_FLAG_HIDE_NAVIGATION
                 | View.SYSTEM_UI_FLAG_IMMERSIVE_STICKY;
         getWindow().getDecorView().setSystemUiVisibility(uiOptions);
+
+        Handler h = new Handler();
+
+        h.postDelayed(new Runnable() {
+            public void run() {
+                ConstraintLayout root = findViewById(R.id.previewPage);
+                goToSucceedPage(root);
+            }
+        }, 8000);
+
+    }
+
+    public void goToSucceedPage(View view) {
+
+        Intent getIntent = getIntent();
+        String userName = getIntent.getStringExtra("userName");
+        String fileName = getIntent.getStringExtra("fileName");
+
+        Intent intent = new Intent(this, succeedActivity.class);
+        intent.putExtra("userName", userName);
+        intent.putExtra("fileName", fileName);
+
+        startActivity(intent);
     }
 }
