c5f4fba045068d04c559bbc57792155735a3f57a
lily-xy <757560761@qq.com>
2022-03-26 00:37:53 +0800

    create  and add functions to the buttons

--- app/src/main/java/com/example/weav/EffectActivity.java
+++ app/src/main/java/com/example/weav/EffectActivity.java
@@ -2,7 +2,10 @@ package com.example.weav;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.LinearLayout;
 
 public class EffectActivity extends AppCompatActivity {
 
@@ -10,5 +13,31 @@ public class EffectActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_effect);
+    }
+
+    public void onClickButtonDeal(View view) {
+
+        Intent getIntent = getIntent();
+        String userName = getIntent.getStringExtra("userName");
+
+        Intent intent = new Intent(this, DealActivity.class);
+        intent.putExtra("userName", userName);
+
+        startActivity(intent);
+    }
+
+    public void showBar(View view) {
+
+        LinearLayout l1 = findViewById(R.id.layout_levelBg);
+        LinearLayout l2 = findViewById(R.id.layout_levelBt);
+
+        if(l1.getTag().equals("inVisible") && l2.getTag().equals("inVisible")){
+
+            l1.setTag("visible");
+            l2.setTag("visible");
+
+            l1.setVisibility(View.VISIBLE);
+            l2.setVisibility(View.VISIBLE);
+        }
     }
 }
