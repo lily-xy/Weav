a4c1997613c2c2ed72faa27a7d3c0dbd28c5bbce
lily-xy <757560761@qq.com>
2022-03-25 12:33:34 +0800

    modify the page's layout and transit the filename to the preview page

--- app/src/main/java/com/example/weav/previewActivity.java
+++ app/src/main/java/com/example/weav/previewActivity.java
@@ -1,14 +1,35 @@
 package com.example.weav;
 
+import androidx.appcompat.app.ActionBar;
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.TextView;
 
 public class previewActivity extends AppCompatActivity {
+
+    private TextView fileName;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_preview);
+
+        ActionBar actionBar = getSupportActionBar();
+        actionBar.hide();
+
+        int uiOptions = View.SYSTEM_UI_FLAG_FULLSCREEN
+                | View.SYSTEM_UI_FLAG_HIDE_NAVIGATION
+                | View.SYSTEM_UI_FLAG_IMMERSIVE_STICKY;
+        getWindow().getDecorView().setSystemUiVisibility(uiOptions);
+
+        fileName = findViewById(R.id.fileName);
+        Intent getIntent = getIntent();
+        String name = getIntent.getStringExtra("fileName");
+        fileName.setText(name);
     }
+
+
 }
