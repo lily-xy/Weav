7bf491865b942f5fc664f7bf92961291205514df
lily-xy <757560761@qq.com>
2022-03-25 21:24:01 +0800

    add Quit function to the button

--- app/src/main/java/com/example/weav/PersonActivity.java
+++ app/src/main/java/com/example/weav/PersonActivity.java
@@ -1,7 +1,10 @@
 package com.example.weav;
 
+import androidx.appcompat.app.AlertDialog;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.constraintlayout.widget.ConstraintLayout;
 
+import android.content.DialogInterface;
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
@@ -35,5 +38,22 @@ public class PersonActivity extends AppCompatActivity {
         intent.putExtra("userName", userName);
 
         startActivity(intent);
+    }
+
+    public void onClickButtonQuit(View view) {
+
+        ConstraintLayout root = findViewById(R.id.personView);
+
+        new AlertDialog.Builder(
+                PersonActivity.this)
+                .setMessage("Are you sure to sign out?")
+                .setPositiveButton("Sure", new DialogInterface.OnClickListener() {
+                    @Override
+                    public void onClick(DialogInterface dialog, int which) {
+                        onClickButtonMain(root);
+                    }
+                })
+                .setNegativeButton("Cancel", null)
+                .show();
     }
 }
