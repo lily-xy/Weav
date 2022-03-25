83c39628e311a2039f096f89b5f7d1ec62deb964
lily-xy <757560761@qq.com>
2022-03-25 11:44:54 +0800

    change the button and create a function to go to the next page for the button

--- app/src/main/java/com/example/weav/DealActivity.java
+++ app/src/main/java/com/example/weav/DealActivity.java
@@ -2,7 +2,10 @@ package com.example.weav;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.ImageView;
 
 public class DealActivity extends AppCompatActivity {
 
@@ -10,5 +13,16 @@ public class DealActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_deal);
+    }
+
+    public void onClickButtonGetFile(View view) {
+
+        Intent getIntent = getIntent();
+        String userName = getIntent.getStringExtra("userName");
+
+        Intent intent = new Intent(this, getFilenameActivity.class);
+        intent.putExtra("userName", userName);
+
+        startActivity(intent);
     }
 }
