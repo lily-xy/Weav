7853d3312207ca08ef4641890924639fe8c6b7e3
lily-xy <757560761@qq.com>
2022-03-25 20:20:15 +0800

    modify the person page's layout and add some functions to the button

--- app/src/main/java/com/example/weav/PersonActivity.java
+++ app/src/main/java/com/example/weav/PersonActivity.java
@@ -2,7 +2,10 @@ package com.example.weav;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.ImageView;
 
 public class PersonActivity extends AppCompatActivity {
 
@@ -10,5 +13,27 @@ public class PersonActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_person);
+    }
+
+    public void onClickButtonWelcome(View view) {
+
+        Intent getIntent = getIntent();
+        String userName = getIntent.getStringExtra("userName");
+
+        Intent intent = new Intent(this, WelcomeActivity.class);
+        intent.putExtra("userName", userName);
+
+        startActivity(intent);
+    }
+
+    public void onClickButtonMain(View view) {
+
+        Intent getIntent = getIntent();
+        String userName = getIntent.getStringExtra("userName");
+
+        Intent intent = new Intent(this, MainActivity.class);
+        intent.putExtra("userName", userName);
+
+        startActivity(intent);
     }
 }
