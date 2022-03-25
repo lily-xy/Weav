33cb8b70ee2b66e7aa4b55de18f1959181c8f128
lily-xy <757560761@qq.com>
2022-03-25 19:19:16 +0800

    change the model of the page and add an id to this page

--- app/src/main/java/com/example/weav/succeedActivity.java
+++ app/src/main/java/com/example/weav/succeedActivity.java
@@ -1,8 +1,10 @@
 package com.example.weav;
 
+import androidx.appcompat.app.ActionBar;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
 
 public class succeedActivity extends AppCompatActivity {
 
@@ -10,5 +12,13 @@ public class succeedActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_succeed);
+
+        ActionBar actionBar = getSupportActionBar();
+        actionBar.hide();
+
+        int uiOptions = View.SYSTEM_UI_FLAG_FULLSCREEN
+                | View.SYSTEM_UI_FLAG_HIDE_NAVIGATION
+                | View.SYSTEM_UI_FLAG_IMMERSIVE_STICKY;
+        getWindow().getDecorView().setSystemUiVisibility(uiOptions);
     }
 }
