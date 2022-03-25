bb99a9bfe2bb6f1229e83db38ad78fd7767ed34e
lily-xy <757560761@qq.com>
2022-03-25 19:30:07 +0800

    add function to edit button

--- app/src/main/java/com/example/weav/succeedActivity.java
+++ app/src/main/java/com/example/weav/succeedActivity.java
@@ -3,8 +3,10 @@ package com.example.weav;
 import androidx.appcompat.app.ActionBar;
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.ImageView;
 
 public class succeedActivity extends AppCompatActivity {
 
@@ -20,5 +22,16 @@ public class succeedActivity extends AppCompatActivity {
                 | View.SYSTEM_UI_FLAG_HIDE_NAVIGATION
                 | View.SYSTEM_UI_FLAG_IMMERSIVE_STICKY;
         getWindow().getDecorView().setSystemUiVisibility(uiOptions);
+    }
+
+    public void onClickButtonGallery(View view) {
+
+            Intent getIntent = getIntent();
+            String userName = getIntent.getStringExtra("userName");
+
+            Intent intent = new Intent(this, GalleryActivity.class);
+            intent.putExtra("userName", userName);
+
+            startActivity(intent);
     }
 }
