264ce5ab5b76eb0fe43ccd5668ab8d296d44e0d0
lily-xy <757560761@qq.com>
2022-03-30 10:01:29 +0800

    write some functions and add them to the buttons

--- app/src/main/java/com/example/weav/ContentActivity.java
+++ app/src/main/java/com/example/weav/ContentActivity.java
@@ -3,6 +3,8 @@ package com.example.weav;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.ImageView;
 
 public class ContentActivity extends AppCompatActivity {
 
@@ -11,4 +13,76 @@ public class ContentActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_content);
     }
+
+    public void cleanChosen(View view) {
+        ImageView img1 = findViewById(R.id.bt1);
+        ImageView img2 = findViewById(R.id.bt2);
+        ImageView img3 = findViewById(R.id.bt3);
+        ImageView bt1 = findViewById(R.id.symmetric1);
+        ImageView bt2 = findViewById(R.id.symmetric2);
+
+        if (img1.getTag() == "chosen"){
+            img1.setImageResource(R.drawable.crop);
+            img1.setTag("unChosen");
+        }
+
+        if (img2.getTag() == "chosen"){
+            img2.setImageResource(R.drawable.eraser);
+            img2.setTag("unChosen");
+        }
+
+        if (img3.getTag() == "chosen"){
+            img3.setImageResource(R.drawable.draw);
+            img3.setTag("unChosen");
+        }
+
+        bt1.setVisibility(View.VISIBLE);
+        bt2.setVisibility(View.VISIBLE);
+    }
+
+    public void choseCrop(View view){
+        ImageView img = findViewById(R.id.bt1);
+        ImageView imgMain = findViewById(R.id.imageMain);
+        ImageView bt1 = findViewById(R.id.symmetric1);
+        ImageView bt2 = findViewById(R.id.symmetric2);
+
+        cleanChosen(view);
+        img.setImageResource(R.drawable.crop_chosen);
+        img.setTag("chosen");
+
+        imgMain.setImageResource(R.drawable.degree);
+        bt1.setImageResource(R.drawable.symmetric1);
+        bt2.setImageResource(R.drawable.symmetric2);
+    }
+
+    public void choseEraser(View view){
+        ImageView img = findViewById(R.id.bt2);
+        ImageView imgMain = findViewById(R.id.imageMain);
+        ImageView bt1 = findViewById(R.id.symmetric1);
+        ImageView bt2 = findViewById(R.id.symmetric2);
+
+        cleanChosen(view);
+        img.setImageResource(R.drawable.eraser_chosen);
+        img.setTag("chosen");
+
+        imgMain.setImageResource(R.drawable.penbold);
+        bt1.setImageResource(R.drawable.pen);
+        bt2.setVisibility(View.INVISIBLE);
+    }
+
+    public void choseWrite(View view){
+        ImageView img = findViewById(R.id.bt3);
+        ImageView imgMain = findViewById(R.id.imageMain);
+        ImageView bt1 = findViewById(R.id.symmetric1);
+        ImageView bt2 = findViewById(R.id.symmetric2);
+
+        cleanChosen(view);
+        img.setImageResource(R.drawable.draw_chosen);
+        img.setTag("chosen");
+
+        imgMain.setImageResource(R.drawable.penbold);
+        bt1.setImageResource(R.drawable.pen);
+        bt2.setVisibility(View.INVISIBLE);
+    }
+
 }
